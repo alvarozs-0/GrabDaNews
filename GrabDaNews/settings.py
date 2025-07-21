e1d786023deddcd3188cb6ad93f75f71b86c96f6
alvarozs-0 <alvarozb2003@gmail.com>
2025-07-21 11:29:40 +0200

    Remove default pagination settings from Django REST Framework configuration

--- GrabDaNews/settings.py
+++ GrabDaNews/settings.py
@@ -160,6 +160,4 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
 }
